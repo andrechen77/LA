6b2eb681fda744f846d3ea2b49c23fdfa699d90e
Andre Chen <101299705+andrechen77@users.noreply.github.com>
2024-02-24 21:24:29 -0600

    add parse tree parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -44,18 +44,349 @@ namespace La::parser {
 		template<typename Separator, typename... Rules>
 		using interleaved = typename interleaved_impl<seq<>, Separator, Rules...>::type;
 
-		struct ProgramRule :
-			seq<
-				TAO_PEGTL_STRING("program")
+		struct CommentRule :
+			disable<
+				TAO_PEGTL_STRING("//"),
+				until<eolf>
 			>
 		{};
 
-		// TODO add all other rules
+		struct SpaceRule :
+			sor<one<' '>, one<'\t'>>
+		{};
+
+		struct SpacesRule :
+			star<SpaceRule>
+		{};
+
+		template<typename... Rules>
+		using spaces_interleaved = interleaved<SpacesRule, Rules...>;
+
+		struct LineSeparatorsRule :
+			star<seq<SpacesRule, eol>>
+		{};
+
+		struct LineSeparatorsWithCommentsRule :
+			star<
+				seq<
+					SpacesRule,
+					sor<eol, CommentRule>
+				>
+			>
+		{};
+
+		struct SpacesOrNewLines :
+			star<sor<SpaceRule, eol>>
+		{};
+
+		struct NameRule :
+			ascii::identifier // the rules for LA names are the same as for C identifiers
+		{};
+
+		struct LabelRule :
+			seq<one<':'>, NameRule>
+		{};
+
+		struct OperatorRule :
+			sor<
+				TAO_PEGTL_STRING("+"),
+				TAO_PEGTL_STRING("-"),
+				TAO_PEGTL_STRING("*"),
+				TAO_PEGTL_STRING("&"),
+				TAO_PEGTL_STRING("<<"),
+				TAO_PEGTL_STRING("<="),
+				TAO_PEGTL_STRING("<"),
+				TAO_PEGTL_STRING(">>"),
+				TAO_PEGTL_STRING(">="),
+				TAO_PEGTL_STRING(">"),
+				TAO_PEGTL_STRING("=")
+			>
+		{};
+
+		struct NumberRule :
+			sor<
+				seq<
+					opt<sor<one<'-'>, one<'+'>>>,
+					range<'1', '9'>,
+					star<digit>
+				>,
+				one<'0'>
+			>
+		{};
+
+		struct InexplicableTRule :
+			sor<
+				NameRule,
+				NumberRule
+			>
+		{};
+
+		struct CallArgsRule :
+			opt<list<
+				InexplicableTRule,
+				one<','>,
+				SpaceRule
+			>>
+		{};
+
+		struct ArrayTypeIndicator :
+			TAO_PEGTL_STRING("[]")
+		{};
+
+		struct TypeRule :
+			sor<
+				seq<
+					rematch<NameRule, TAO_PEGTL_STRING("int64")>,
+					star<ArrayTypeIndicator>
+				>,
+				rematch<NameRule, TAO_PEGTL_STRING("tuple")>,
+				rematch<NameRule, TAO_PEGTL_STRING("code")>,
+				rematch<NameRule, TAO_PEGTL_STRING("void")>
+			>
+		{};
+
+		struct NonVoidTypeRule :
+			minus<TypeRule, TAO_PEGTL_STRING("void")>
+		{};
+
+		struct IndexingExpressionRule :
+			spaces_interleaved<
+				NameRule,
+				star<
+					one<'['>,
+					InexplicableTRule,
+					one<']'>
+				>
+			>
+		{};
+
+		struct CallingExpressionRule :
+			spaces_interleaved<
+				NameRule,
+				one<'('>,
+				CallArgsRule,
+				one<')'>
+			>
+		{};
+
+		struct InstructionDeclarationRule :
+			spaces_interleaved<
+				NonVoidTypeRule,
+				NameRule
+			>
+		{};
+
+		struct ArrowSymbolRule : TAO_PEGTL_STRING("\x3c-") {};
+
+		struct InstructionOpAssignmentRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				InexplicableTRule,
+				OperatorRule,
+				InexplicableTRule
+			>
+		{};
+
+		struct InstructionReadTensorRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				IndexingExpressionRule
+			>
+		{};
+
+		struct InstructionWriteTensorRule :
+			spaces_interleaved<
+				IndexingExpressionRule,
+				ArrowSymbolRule,
+				InexplicableTRule
+			>
+		{};
+
+		struct InstructionGetLengthRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				TAO_PEGTL_STRING("length"),
+				NameRule,
+				opt<InexplicableTRule>
+			>
+		{};
+
+		struct InstructionCallVoidRule :
+			spaces_interleaved<
+				CallingExpressionRule
+			>
+		{};
+
+		struct InstructionCallValRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				CallingExpressionRule
+			>
+		{};
+
+		struct InstructionNewArrayRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				TAO_PEGTL_STRING("new"),
+				TAO_PEGTL_STRING("Array"),
+				one<'('>,
+				CallArgsRule,
+				one<')'>
+			>
+		{};
+
+		struct InstructionNewTupleRule :
+			spaces_interleaved<
+				NameRule,
+				ArrowSymbolRule,
+				TAO_PEGTL_STRING("new"),
+				TAO_PEGTL_STRING("Tuple"),
+				one<'('>,
+				CallArgsRule,
+				one<')'>
+			>
+		{};
+
+		struct InstructionLabelRule :
+			spaces_interleaved<
+				LabelRule
+			>
+		{};
+
+		struct InstructionBranchUncondRule :
+			spaces_interleaved<
+				TAO_PEGTL_STRING("br"),
+				LabelRule
+			>
+		{};
+
+		struct InstructionBranchCondRule :
+			spaces_interleaved<
+				TAO_PEGTL_STRING("br"),
+				InexplicableTRule,
+				LabelRule,
+				LabelRule
+			>
+		{};
+
+		struct InstructionReturnRule :
+			spaces_interleaved<
+				TAO_PEGTL_STRING("return"),
+				opt<InexplicableTRule>
+			>
+		{};
+
+		struct InstructionRule :
+			sor<
+				InstructionDeclarationRule,
+				InstructionGetLengthRule,
+				InstructionNewArrayRule,
+				InstructionNewTupleRule,
+				InstructionCallVoidRule,
+				InstructionCallValRule,
+				InstructionOpAssignmentRule,
+				InstructionReadTensorRule,
+				InstructionWriteTensorRule,
+				InstructionLabelRule,
+				InstructionBranchUncondRule,
+				InstructionBranchCondRule,
+				InstructionReturnRule
+			>
+		{};
+
+		struct InstructionsRule :
+			opt<list<
+				seq<bol, SpacesRule, InstructionRule>,
+				LineSeparatorsWithCommentsRule
+			>>
+		{};
+
+		struct DefArgRule :
+			spaces_interleaved<
+				NonVoidTypeRule,
+				NameRule
+			>
+		{};
+
+		struct DefArgsRule :
+			opt<list<
+				DefArgRule,
+				one<','>,
+				SpaceRule
+			>>
+		{};
+
+		struct FunctionDefinitionRule :
+			interleaved<
+				LineSeparatorsWithCommentsRule,
+				interleaved<
+					SpacesOrNewLines,
+					TypeRule,
+					NameRule,
+					one<'('>,
+					DefArgsRule,
+					one<')'>,
+					one<'{'>
+				>,
+				InstructionsRule,
+				seq<SpacesRule, one<'}'>>
+			>
+		{};
+
+		struct ProgramRule :
+			list<
+				seq<SpacesRule, FunctionDefinitionRule>,
+				LineSeparatorsWithCommentsRule
+			>
+		{};
+
+		struct ProgramFile :
+			seq<
+				bof,
+				LineSeparatorsWithCommentsRule,
+				ProgramRule,
+				LineSeparatorsWithCommentsRule,
+				eof
+			>
+		{};
 
 		template<typename Rule>
 		struct Selector : pegtl::parse_tree::selector<
 			Rule,
 			pegtl::parse_tree::store_content::on<
+				NameRule,
+				OperatorRule,
+				NumberRule
+			>,
+			pegtl::parse_tree::remove_content::on<
+				LabelRule,
+				CallArgsRule,
+				ArrayTypeIndicator,
+				TypeRule,
+				IndexingExpressionRule,
+				CallingExpressionRule,
+				DefArgRule,
+				DefArgsRule,
+				InstructionDeclarationRule,
+				InstructionOpAssignmentRule,
+				InstructionReadTensorRule,
+				InstructionWriteTensorRule,
+				InstructionGetLengthRule,
+				InstructionCallVoidRule,
+				InstructionCallValRule,
+				InstructionNewArrayRule,
+				InstructionNewTupleRule,
+				InstructionLabelRule,
+				InstructionBranchUncondRule,
+				InstructionBranchCondRule,
+				InstructionReturnRule,
+				InstructionsRule,
+				FunctionDefinitionRule,
 				ProgramRule
 			>
 		> {};
@@ -117,6 +448,11 @@ namespace La::parser {
 			return this->end.data != nullptr;
 		}
 
+		template<typename... States>
+		void remove_content(States &&... /*unused*/) {
+			this->end = TAO_PEGTL_NAMESPACE::internal::inputerator();
+		}
+
 		bool is_root() const noexcept {
 			return static_cast<bool>(this->rule);
 		}
@@ -130,7 +466,7 @@ namespace La::parser {
 	}
 
 	Uptr<La::program::Program> parse_file(char *fileName, Opt<std::string> parse_tree_output) {
-		using EntryPointRule = pegtl::must<rules::ProgramRule>;
+		using EntryPointRule = pegtl::must<rules::ProgramFile>;
 
 		// Check the grammar for some possible issues.
 		// TODO move this to a separate file bc it's performance-intensive
